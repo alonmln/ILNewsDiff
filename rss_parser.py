35a10786599639ff29577d5b6b7f881113d913c0
Alon <alonmln@gmail.com>
2021-01-31 20:14:00 +0200

    Add logging to find the issue

--- rss_parser.py
+++ rss_parser.py
@@ -12,6 +12,12 @@ class RSSParser(BaseParser):
 
     def parse(self):
         r = feedparser.parse(self.url)
+        import urllib.request
+        with urllib.request.urlopen(self.url) as response:
+            data = response.read()
+            import datetime
+            with open(f"./{self.get_source()}_{str(datetime.datetime.now()).replace(':','')}.xml", "wb") as f:
+                f.write(data)
         if r is None:
             logging.warning('Empty response RSS')
             return
