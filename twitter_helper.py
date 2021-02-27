5c3eeb9cf6951e58d83a086cde18137f113ba980
Alon <alonmln@gmail.com>
2021-02-27 09:48:30 +0200

    Added success log

--- twitter_helper.py
+++ twitter_helper.py
@@ -29,6 +29,7 @@ def upload_media(filename):
         response = twitter_api.media_upload(filename)
         logging.debug("Upload successful, deleting diff image file")
         os.remove(filename)
+        logging.debug("Diff image deleted successfully")
     except Exception:
         print(sys.exc_info()[0])
         logging.exception('Media upload')
